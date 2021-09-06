bdbae062fc7450814c50948ab483756bc58818e0
Muhammad Hamza Tiwana <tiv.hamza123@gmail.com>
2021-09-06 17:43:30 +0500

    Repository updated on Monday

--- src/App.js
+++ src/App.js
@@ -2,9 +2,40 @@ import './App.css';
 import Header from "./MyComponents/Header";
 import {ToDos} from "./MyComponents/ToDos";
 import {Footer} from "./MyComponents/Footer";
+import {AddToDo} from "./MyComponents/AddToDo";
+import React, { useState } from 'react';
+
 
 function App() {
-  let todos = [
+  const onDelete = (todo)=>{
+    console.log("I am onDelete of todo", todo);
+    // Deleting this way in react doesn't work
+    // let index = todos.indexOf(todo);
+    // todos.splice(index, 1);
+
+    setTodos(todos.filter((e) =>{
+      return e!==todo;
+    }))
+  };
+const addTodo = (title, desc)=>{
+  console.log("I am adding this todo",title,desc);
+  let sno;
+  if(todos.length==0){
+    sno = 0;
+  }else{
+    sno = todos[todos.length-1].sno + 1;
+  }
+  const mytodo = {
+    sno: sno,
+    title: title,
+    desc: desc,
+  }
+  setTodos([...todos, mytodo]);
+  console.log(mytodo);
+
+}
+
+  const [todos, setTodos] = useState([
     {
     sno: 1,
     title: "Go to the Market",
@@ -19,12 +50,14 @@ function App() {
     sno: 3,
     title: "Go to Office",
     desc: "You need to go to the office to learn ReactJS"
-    }
-  ]
+    },
+  ])
+   
   return (
     <>
-    <Header title="My Todos List" />
-    <ToDos todos = {todos}/>
+    <Header title="My Todos List" searchBar = {true} />
+    <AddToDo addTodo ={addTodo}/>
+    <ToDos todos = {todos} onDelete={onDelete}/>
     <Footer/>
     </>
   );
--- src/MyComponents/ToDos.js
+++ src/MyComponents/ToDos.js
@@ -2,10 +2,23 @@ import React from 'react'
 import {ToDoItems} from "../MyComponents/ToDoItems";
 
 export const ToDos = (props) => {
+    let myStyle = {
+        minHeight: "100vh",
+        margin: "50px auto"
+    }
+
     return (
-        <div className = "container">
-            <h3>ToDos List</h3>
-            <ToDoItems todo ={props.todos[0]}/>
+        <div className = "container" style = {myStyle}> 
+            <h3 className="text-center my-3">ToDos List</h3>
+            {props.todos.length===0? "No Todos to Display":
+            props.todos.map((todo)=> {
+                return (
+                <>
+                <ToDoItems todo ={todo} key = {todo.sno}  onDelete={props.onDelete} />
+                <hr/>
+                </>
+                )
+            })}
         </div>
     )
 }
