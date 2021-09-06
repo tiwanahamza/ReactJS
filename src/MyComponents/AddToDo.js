d756846855db5a6af0168a5c4a32f05437522bc0
Muhammad Hamza Tiwana <tiv.hamza123@gmail.com>
2021-09-06 17:44:56 +0500

    Added a new file AddToDo.js

---
+++ src/MyComponents/AddToDo.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+export const AddToDo = (props) => {
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+
+    const submit = (e)=>{
+        e.preventDefault();
+        if (!title || !desc){
+            alert("Title or Description can not be blank");
+        }
+        props.addTodo(title, desc);
+    }
+    return (
+        <div className = "container">
+            <h3 className = "text-center">Add a Todo</h3>
+            <form onSubmit = {submit}>
+                <div className="mb-3">
+                    <label htmlFor="title" className="form-label">ToDo Title</label>
+                    <input type="text" value = {title} onChange = {(e)=> {
+                        setTitle(e.target.value)
+                    }} 
+                     className="form-control" id="title" aria-describedby="emailHelp"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="desc" className="form-label">ToDo Description</label>
+                    <input type="text" value = {desc} onChange = {(e)=> {
+                        setDesc(e.target.value)
+                    }} className="form-control" id="desc"/>
+                </div>
+                <button type="submit" className="btn btn-success btn-sm ">Submit</button>
+            </form>
+        </div>
+    )
+}
